92daaf1b91666bda69c5d15697948a8ded216676
Anthony Phillips <github.com@tonybphillips.com>
2012-05-14 13:53:36 -0700

    clue_4_reset

--- read_secret_note.rb
+++ read_secret_note.rb
@@ -5,7 +5,13 @@ puts "what is the password?"
 print "password:"
 
 password = gets.chomp
-
+["Reset_to_get_rid_of_me.rb", "files_have_been_created.rb", "these_changes_broke_the_code.rb", "who_knows_what_i_did_to_break_everything.rb", "I_am_a_piece_of_the_secret_note.rb", "I_am_another_piece_of_the_secret_note.rb"].each do |title|
+	if File.exists?(title)
+		"Uh oh.  It seems #{title} still exists.  Try adding the file and then resetting again." 
+		@hidden = "Reset after resetting completely back to the last commit."		
+		return
+	end
+end
 if password == @password
 	puts @hidden
 else
