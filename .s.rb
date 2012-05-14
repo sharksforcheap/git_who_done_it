ed7f17f6299c85cbc354fd7dfa9c3a2d28a7cf3d
Anthony Phillips <github.com@tonybphillips.com>
2012-05-14 13:38:27 -0700

    clue_4_reset

--- .s.rb
+++ .s.rb
@@ -4,3 +4,4 @@
 	Yes, you caught me! My motive was jealousy, but it wasn't as bad as that man's blackmail motive, who entered at five minutes past nine. I entered before another man who left the incriminating evidence of the cached page.
 
 	Miss Sass"
+
