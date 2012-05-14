d3ca0c378b3931442c86de8f2917a1d2b3900f5f
Anthony Phillips <github.com@tonybphillips.com>
2012-05-14 03:02:05 -0700

    clue_5_merge_conflicts

--- the_file_you_and_your_partner_edited.rb
+++ the_file_you_and_your_partner_edited.rb
@@ -1,5 +1,7 @@
 require '.m.rb'
 
 puts "We have received an annonymous tip that reads the following:"
+puts @important_thing
 puts "It also says:"
+puts @why_did_we_write_this_down
 puts "I hope this information comes in handy."
