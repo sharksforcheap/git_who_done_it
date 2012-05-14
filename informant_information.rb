c35ae259000806f18dd7029b88d72c86f41cb648
Anthony Phillips <github.com@tonybphillips.com>
2012-05-14 09:56:06 -0700

    clue_2_merge_recursive

---
+++ informant_information.rb
@@ -0,0 +1,3 @@
+def speak
+	puts 'I heard Lord Bash tell someone: "I entered after a woman, who did not take in rope because the last person to visit him did. I was in there for more than thirty-five minutes confronting Shereef with my motive, which, may I say, wasn\'t greed or blackmail."'
+end
